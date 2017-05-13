a7a8bea08b0d8183d224ac66084477dd8c85d1bc
Munchman <league@Admins-Mac-mini.local>
2017-05-13 11:27:30 -0700

    hello my name is paola I’m in your code class

---
+++ src/knockknock.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class knockknock {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("knock knock");
+	JOptionPane.showInputDialog("daisy");
+	JOptionPane.showInputDialog("dasi me rollin they hatin ");
+}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/morningZombie.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class morningZombie {
+public static void main(String[] args) {
+	System.out.println("hi im tierd ");
+	JOptionPane.showMessageDialog(null, "hi");
+	JOptionPane.showMessageDialog(null, "hi how are you");
+	JOptionPane.showMessageDialog(null, "like your hair");
+	JOptionPane.showMessageDialog(null, "your cool");
+	JOptionPane.showMessageDialog(null, "do you have a dog???");
+	JOptionPane.showMessageDialog(null, "stop doing what your doing");
+	JOptionPane.showMessageDialog(null, "like your glasses");
+	JOptionPane.showMessageDialog(null, "have a nice day");
+	JOptionPane.showMessageDialog(null, "wake up");
+	JOptionPane.showMessageDialog(null, "change your clothes");
+	JOptionPane.showMessageDialog(null, "brush your teeth");
+	JOptionPane.showMessageDialog(null, "get ready to eat your breakfast");
+	JOptionPane.showMessageDialog(null, "bus is here get ready to go to school");
+	JOptionPane.showMessageDialog(null, "have a nice day");
+}
+}
