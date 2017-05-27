f69ed5ad7b5645374e2dc4baf0208c241fe5b6d8
paolapadilla <league@Mac-mini-4.local>
2017-05-27 11:27:14 -0700

    we did riddles

---
+++ src/Riddler.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Riddler {
+	public static void main(String[] args) {
+	JOptionPane.showInputDialog("what comes down but never goes up?");
+	JOptionPane.showMessageDialog(null, "rain");
+	JOptionPane.showInputDialog("Q: I’m tall when I’m young and I’m short when I’m old. What am I?");
+	JOptionPane.showMessageDialog(null, "a candle");
+	JOptionPane.showInputDialog(" What has a foot but no legs");
+	JOptionPane.showMessageDialog(null, "A snail");
+	JOptionPane.showInputDialog("What has one eye but cannot see?");
+	JOptionPane.showMessageDialog(null, "a needle");
+	}
+
+}
---
+++ src/Warmup.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Warmup {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"hello my name is paola");
+		JOptionPane.showMessageDialog(null, "i like monkeys");
+		JOptionPane.showMessageDialog(null, "i like sleeping in bed");
+		JOptionPane.showMessageDialog(null, "i like my dog rocko");
+		JOptionPane.showMessageDialog(null, "i like FOOD");
+		JOptionPane.showMessageDialog(null, "i like knots berry farm  and universal studios");
+	}
+}
