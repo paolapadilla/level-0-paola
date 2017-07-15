d5096ab9ed3f6831bb9c8e9fcab2d8b4825cdb74
paolapadilla <league@WorthyMini.local>
2017-07-15 11:34:47 -0700

    it was cool todays class was cool

---
+++ src/Randp.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Randp {
+public static void main(String[] args) {
+	
+	JOptionPane.showMessageDialog(null, "rocko is my dog");
+	String name=JOptionPane.showInputDialog("whats your favorite food ");
+	JOptionPane.showInputDialog(name+" cool");
+	
+	
+}
+}
---
+++ src/creeper.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class creeper {
+public static void main(String[] args) {
+String name=JOptionPane.showInputDialog("hi my name is bob whats yours");
+String address=JOptionPane.showInputDialog("i work in apple and we have an offer going on in black friday we want to know if you will like a free iphone 7 plus but to get the iphone you have to give me your address and your pin number ");
+String phonenumber=JOptionPane.showInputDialog("thanks for everthing" + name + "also i dont work in apple also im going to take your money and use it to buy myself a iphone 7 plus see you later person");
+}
+}
