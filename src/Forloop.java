26f3d4aea58d7d3400f3f7421e002413bdf5fb1e
paolapadilla <league@WTS-MM-7.local>
2017-11-18 11:28:24 -0800

    i keep learning each class

--- src/Forloop.java
+++ src/Forloop.java
@@ -1,10 +1,26 @@
 
 public class Forloop {
 public static void main(String[] args) {
-		for(int i=1;i<10;i++){
+		for(int i=75;i>50;i--){
 			
 			System.out.println(i);
 }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 }
 
 	
