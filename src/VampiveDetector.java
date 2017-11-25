b4a2ee8a67da7a5fcb368a2817b23706352f25bb
paolapadilla <league@Mac-mini-4.local>
2017-11-25 11:35:54 -0800

    i learned how to do modulo

---
+++ src/VampiveDetector.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class VampiveDetector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String garlic =JOptionPane.showInputDialog("do you like garlic?");
+
+String light =JOptionPane.showInputDialog("do you like sun lights?");
+
+String age =JOptionPane.showInputDialog("how old are you ?");
+
+if( garlic.equals("no")){
+	  
+}
+	}
+
+}
