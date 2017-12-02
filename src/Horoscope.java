7f040589fe000d6383427511bea4b39c7512170d
paolapadilla <league@FunkyMini.local>
2017-12-02 11:29:08 -0800

    i did the vampire detector and then i moved to the Horoscope

---
+++ src/Horoscope.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Horoscope {
+
+	public static void main(String[] args) {
+
+		String starsign = JOptionPane.showInputDialog("What is your star sign?");
+if (starsign.equals"Aries"){
+	
+}
+	}
+
+}
--- src/VampiveDetector.java
+++ src/VampiveDetector.java
@@ -3,16 +3,35 @@ import javax.swing.JOptionPane;
 public class VampiveDetector {
 
 	public static void main(String[] args) {
+		boolean vampire = false;
 		// TODO Auto-generated method stub
-String garlic =JOptionPane.showInputDialog("do you like garlic?");
+		String garlic = JOptionPane.showInputDialog("do you like garlic?");
 
-String light =JOptionPane.showInputDialog("do you like sun lights?");
+		String light = JOptionPane.showInputDialog("do you like sun lights?");
 
-String age =JOptionPane.showInputDialog("how old are you ?");
+		String age = JOptionPane.showInputDialog("how old are you ?");
 
-if( garlic.equals("no")){
-	  
-}
+		int old = Integer.parseInt(age);
+
+		if (garlic.equals("no") && light.equals("no") && old > 100) {
+			vampire = true;
+		}
+
+		else if (garlic.equals("no") && light.equals("yes") && old < 100) {
+			vampire = false;
+		} else if (garlic.equals("yes") && light.equals("no") && old < 100) {
+			vampire = false;
+		} else if (garlic.equals("yes") && light.equals("yes") && old < 100) {
+			vampire = false;
+		}else if (old >100){
+			vampire=true;
+		}
+		if (vampire == true) {
+			JOptionPane.showMessageDialog(null, "your're a vampire GET OUT!!!!");
+		} else {
+			JOptionPane.showMessageDialog(null, "your're not a vampire your hired");
+		}
+
 	}
 
 }
