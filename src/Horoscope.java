f348ff2dccaf13b0159a1ed5867adcf50a5665f2
paolapadilla <league@WorthyMini-3.local>
2017-12-09 11:33:26 -0800

    Im still working on Horoscope

--- src/Horoscope.java
+++ src/Horoscope.java
@@ -5,9 +5,22 @@ public class Horoscope {
 	public static void main(String[] args) {
 
 		String starsign = JOptionPane.showInputDialog("What is your star sign?");
-if (starsign.equals"Aries"){
-	
-}
-	}
+		if (starsign.equals("Aries")) {
+			JOptionPane.showMessageDialog(null, "Aries is full of life and possesses high energy");
 
+		} else if (starsign.equals("Taurus")) {
+			JOptionPane.showMessageDialog(null, "your caracteristics is to be peaceful and methodical");
+		} else if (starsign.equals("Gemini")) {
+			JOptionPane.showMessageDialog(null, "you rarely like to do anything alone");
+
+		} else if (starsign.equals("Cancer")) {
+			JOptionPane.showMessageDialog(null, " you have a sence of humor");
+		}else if (starsign.equals("Leo")){
+			JOptionPane.showMessageDialog(null, "you are relax,proud,in charge,and regal");
+		}else if (starsign.equals("Virgo")){
+			JOptionPane.showMessageDialog(null, " you have the heart for their fellow man");
+		} else if (starsign.equals("Libra")){
+			JOptionPane.showMessageDialog(null, " you have sence of fairness");
+		}else if (starsign.equals(""))
+	}
 }
--- src/knockknock.java
+++ src/knockknock.java
@@ -4,7 +4,7 @@ public class knockknock {
 public static void main(String[] args) {
 	JOptionPane.showInputDialog("knock knock");
 	JOptionPane.showInputDialog("daisy");
-	JOptionPane.showInputDialog("dasi me rollin they hatin ");
+	JOptionPane.showMessageDialog(null,"daisy me rollin they hatin ");
 }
 	
 	
