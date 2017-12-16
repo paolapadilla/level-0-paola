a2e0d37313720453957c87b76c3ab3452fde421b
paolapadilla <league@WorthyMini.local>
2017-12-16 11:30:24 -0800

    i finished horoscope but the computer went to sleep and shut down on me

--- src/Horoscope.java
+++ src/Horoscope.java
@@ -5,22 +5,24 @@ public class Horoscope {
 	public static void main(String[] args) {
 
 		String starsign = JOptionPane.showInputDialog("What is your star sign?");
-		if (starsign.equals("Aries")) {
+		if (starsign.equalsIgnoreCase("Aries")) {
 			JOptionPane.showMessageDialog(null, "Aries is full of life and possesses high energy");
 
-		} else if (starsign.equals("Taurus")) {
+		} else if (starsign.equalsIgnoreCase("Taurus")) {
 			JOptionPane.showMessageDialog(null, "your caracteristics is to be peaceful and methodical");
-		} else if (starsign.equals("Gemini")) {
+		} else if (starsign.equalsIgnoreCase("Gemini")) {
 			JOptionPane.showMessageDialog(null, "you rarely like to do anything alone");
 
-		} else if (starsign.equals("Cancer")) {
+		} else if (starsign.equalsIgnoreCase("Cancer")) {
 			JOptionPane.showMessageDialog(null, " you have a sence of humor");
-		}else if (starsign.equals("Leo")){
+		}else if (starsign.equalsIgnoreCase("Leo")){
 			JOptionPane.showMessageDialog(null, "you are relax,proud,in charge,and regal");
-		}else if (starsign.equals("Virgo")){
+		}else if (starsign.equalsIgnoreCase("Virgo")){
 			JOptionPane.showMessageDialog(null, " you have the heart for their fellow man");
-		} else if (starsign.equals("Libra")){
+		} else if (starsign.equalsIgnoreCase("Libra")){
 			JOptionPane.showMessageDialog(null, " you have sence of fairness");
-		}else if (starsign.equals(""))
+		}else if (starsign.equalsIgnoreCase("scorpio")){
+			JOptionPane.showMessageDialog(null, "you are protective and are very intrested in others.");
+		}
 	}
 }
