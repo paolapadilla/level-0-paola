d3f2af22d76a5ff66444c773e3046a8c51829bf0
paolapadilla <league@WorthyMini.local>
2018-01-20 11:26:45 -0800

    I'm working on the obedient robot in the circle

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -1,21 +1,30 @@
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-	 static Robot potato = new Robot();
+	static Robot potato = new Robot();
+
 	public static void main(String[] args) {
-		drawSquare();
-potato.penUp();
-potato.move(300);
-potato.penDown();
-potato.turn(45);
-potato.move(100);
-potato.turn(90);
-potato.move(100);
-potato.turn(130);
+		//drawSquare();
+		//drawtriangle();
+		drawcircle();
+	}
+		private static void drawtriangle(){
+		potato.penUp();
+		potato.setSpeed(100);
+		potato.move(300);
+		potato.penDown();
+		potato.turn(46);
+		potato.move(100);
+		potato.turn(90);
+		potato.move(100);
+		potato.turn(130);
+		potato.move(150);
 
 	}
+
 	private static void drawSquare() {
 		potato.penDown();
+		potato.setSpeed(100);
 		potato.move(200);
 		potato.turn(90);
 		potato.move(200);
@@ -24,6 +33,15 @@ potato.turn(130);
 		potato.turn(90);
 		potato.move(200);
 		potato.turn(90);
+		potato.move(100);
 	}
 
+	private static void drawcircle() {
+		potato.penUp();
+		potato.setSpeed(100);
+		potato.penDown();
+		for(int i=0;i<10;i++)
+		potato.turn(-60);
+		
+	}
 }
