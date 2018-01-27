c11e805c23144edbe1bb43ac24d56123585eba14
tactreconx <league@Admins-Mac-mini-2.local>
2018-01-27 11:24:11 -0800

    i finished obedientRobot and I'm working on Bulleyes

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -1,47 +1,63 @@
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
 	static Robot potato = new Robot();
 
 	public static void main(String[] args) {
-		//drawSquare();
-		//drawtriangle();
-		drawcircle();
+		potato.setSpeed(10);
+
+		String rocko = JOptionPane.showInputDialog("What shape do you want Triangle, Square, or Circle?");
+		String cat = JOptionPane.showInputDialog("What color do you want red, green, or blue?");
+		if (cat.equalsIgnoreCase("red")) {
+			potato.setPenColor(Color.red);
+		} else if (cat.equalsIgnoreCase("green")) {
+			potato.setPenColor(Color.green);
+		} else if (cat.equalsIgnoreCase("blue")) {
+			potato.setPenColor(Color.BLUE);
+		}
+
+		if (rocko.equalsIgnoreCase("Triangle")) {
+			drawtriangle();
+		} else if (rocko.equalsIgnoreCase("Square")) {
+			drawSquare();
+		} else if (rocko.equalsIgnoreCase("Circle")) {
+			drawcircle();
+		}
+
 	}
-		private static void drawtriangle(){
+
+	private static void drawtriangle() {
 		potato.penUp();
-		potato.setSpeed(100);
-		potato.move(300);
 		potato.penDown();
-		potato.turn(46);
-		potato.move(100);
-		potato.turn(90);
-		potato.move(100);
-		potato.turn(130);
-		potato.move(150);
+		for(int i =0;i<3;i++) {
+			potato.move(100);
+			potato.turn(120);
+		}
+	
 
 	}
 
 	private static void drawSquare() {
 		potato.penDown();
-		potato.setSpeed(100);
-		potato.move(200);
-		potato.turn(90);
-		potato.move(200);
-		potato.turn(90);
-		potato.move(200);
-		potato.turn(90);
-		potato.move(200);
-		potato.turn(90);
-		potato.move(100);
+		for (int i = 0; i < 4; i++) {
+			potato.move(200);
+			potato.turn(90);
+		}
+		
+		
 	}
 
 	private static void drawcircle() {
 		potato.penUp();
-		potato.setSpeed(100);
 		potato.penDown();
-		for(int i=0;i<10;i++)
-		potato.turn(-60);
-		
+		for (int i = 0; i < 360; i++) {
+			potato.move(1);
+			potato.turn(1);
+		}
+
 	}
 }
