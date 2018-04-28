944bcb3c32acf6e69b8cb2edeb25ac9490d90da8
paolapadilla <league@Mac-mini-3.local>
2018-04-28 11:19:56 -0700

    i finished test scores and I'm working on Robot graffiti

--- src/TestScores.java
+++ src/TestScores.java
@@ -4,11 +4,16 @@ public class TestScores {
 
 	public static void main(String[] args) {
 		String wendy =JOptionPane.showInputDialog("what was your score in a test that you took??");
-		int sky=Integer.parseInt(wendy);
-		if(sky>80) {
+		double sky=Double.parseDouble(wendy);
+		if(sky>=80&&sky<=100) {
 			JOptionPane.showMessageDialog(null, "wow,you must have studied hard!!");
+		}if(sky<=79&&sky>=0) {
+			JOptionPane.showMessageDialog(null, "Try harder next time!");
 		}
-
+	if(sky>100) {
+		JOptionPane.showMessageDialog(null, "please enter a real test score between 1-100%");
 	}
 
 }
+}
+
