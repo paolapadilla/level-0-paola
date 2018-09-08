2b00d92af7d6ba9c43417fe2acaf8e5fe1b4424e
paolapadilla <league@WTS-MM-7.local>
2018-09-08 11:23:33 -0700

    Im reviewing and doing some code on last Summer

---
+++ src/LastSummer.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class LastSummer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+JOptionPane.showInputDialog("What is your Name");
+String=Rocko
+
+
+	}
+
+}
